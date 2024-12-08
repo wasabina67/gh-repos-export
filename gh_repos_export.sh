049d0bf7ce793ae2e7ab211dc3b95fdfaf0f3671
wasabina67 <wasabina67@gmail.com>
2024-12-08 23:32:10 +0900

    Update gh_repos_export.sh

--- gh_repos_export.sh
+++ gh_repos_export.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 username=wasabina67
-gh repo list ${username} --json name,isPrivate --limit 50 | jq -r 'sort_by(.name)  | .[] | [.name, .isPrivate] | @csv' > repos.csv
+gh repo list ${username} --json name,isPrivate --limit 100 | jq -r 'sort_by(.name)  | .[] | [.name, .isPrivate] | @csv' > repos.csv
+# sed -i 's/"//g' repos.csv
